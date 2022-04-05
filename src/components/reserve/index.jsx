fc415d2092857aec6ae7e886aef79b80d24b17d5
Mario Conti <marioconti91@gmail.com>
2022-04-05 13:28:54 -0300

    prueba

--- src/components/reserve/index.jsx
+++ src/components/reserve/index.jsx
@@ -1,34 +1,34 @@
-import React, { useState } from "react";
-import { styled, Box } from "@mui/system";
-import ModalUnstyled from "@mui/core/ModalUnstyled";
+import React from "react";
+// import { styled, Box } from "@mui/system";
+// import ModalUnstyled from "@mui/core/ModalUnstyled";
 import "./styles.css";
 
-const StyledModal = styled(ModalUnstyled)`
-  position: fixed;
-  z-index: 1300;
-  right: 0;
-  bottom: 0;
-  top: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+// const StyledModal = styled(ModalUnstyled)`
+//   position: fixed;
+//   z-index: 1300;
+//   right: 0;
+//   bottom: 0;
+//   top: 0;
+//   left: 0;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+// `;
 
-const Backdrop = styled("div")`
-  z-index: -1;
-  position: fixed;
-  right: 0;
-  bottom: 0;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  -webkit-tap-highlight-color: transparent;
-`;
+// const Backdrop = styled("div")`
+//   z-index: -1;
+//   position: fixed;
+//   right: 0;
+//   bottom: 0;
+//   top: 0;
+//   left: 0;
+//   background-color: rgba(0, 0, 0, 0.5);
+//   -webkit-tap-highlight-color: transparent;
+// `;
 
 export const Reserve = () => {
-  const [open, setOpen] = useState(false);
-  const handleButton = () => setOpen(!open);
+  // const [open, setOpen] = useState(false);
+  // const handleButton = () => setOpen(!open);
 
   return (
     <div id="confirmation" className="container-reserve">
