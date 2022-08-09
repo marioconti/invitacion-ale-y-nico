df81ddf2dbe06b98e69452581847b80a71bbbece
Mario Conti <marioconti91@gmail.com>
2022-08-09 10:28:38 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { NavBar } from "./components/navbar";
 import { Home } from "./components/home";
 import { Reserve } from "./components/reserve";
 import { Location } from "./components/location";
-import Maps from "./components/maps";
+// import Maps from "./components/maps";
 import { Gifts } from "./components/gifts";
 import { ListWedding } from "./components/musicList";
 import { Gratitude } from "./components/gratitude";
